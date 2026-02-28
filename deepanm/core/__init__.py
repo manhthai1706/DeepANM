b5cdd78a4239e05ec5ec498e1b7ee178473a6e26
oh no <manh.thai.170@gmail.com>
2026-02-28 08:35:46 +0700

    remove: delete kernels.py - RBFKernel never used internally, pipeline uses RFFGPLayer

--- deepanm/core/__init__.py
+++ deepanm/core/__init__.py
@@ -4,13 +4,11 @@ Contains fundamental components for causal discovery
 """
 
 from .gppom_hsic import GPPOMC_lnhsic_Core, FastHSIC, RFFGPLayer
-from .kernels import RBFKernel
 from .mlp import MLP
 
 __all__ = [
     'GPPOMC_lnhsic_Core',
     'FastHSIC',
     'RFFGPLayer',
-    'RBFKernel',
     'MLP'
 ]
